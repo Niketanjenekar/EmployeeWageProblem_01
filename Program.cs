e4c2bdf5e8f04a7149495d62d3a53bc502ab3488
Niketanjenekar <97117476+Niketanjenekar@users.noreply.github.com>
2023-03-09 10:59:45 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee\r\nWage Computation Program");
-            int is_present = 1;
-            EmployeeCheck.EmployeeAttendence(is_present);
+           
         }
     }
 }
