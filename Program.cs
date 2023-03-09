7fef2f37185be84fbe07feaa613ccc134a7a05b9
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-09 10:57:12 +0530

    Employee Wage problem first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputationalProblem
+{
+    public class EmployeeWageProblem
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee\r\nWage Computation Program");
+            int is_present = 1;
+            EmployeeCheck.EmployeeAttendence(is_present);
+        }
+    }
+}
