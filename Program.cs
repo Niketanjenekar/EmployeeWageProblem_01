472c365100847ae63dc802fd7efb9f150614b6e4
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-10 23:43:14 +0530

    Employee Daily Wage Use Case 2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Welcome to Employee\r\nWage Computation Program");
             EmployeeCheck.EmployeeAttendence();
+            EmployeedailyWageProblem.DailyEmployeeWage();
         }
     }
 }
