fb00f09c55e979a65cf5d70721674b99fb9b71e0
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-11 12:42:04 +0530

    Calculating Wages of Employees For a Month

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome to Employee Wage Computation Program : ");
             //  EmployeeCheck.EmployeeAttendence();
             //  EmployeedailyWageProblem.DailyEmployeeWage();
             //  EmployeePartTime.EmployeedailyPartTime();
-            EmployeeWageUsingSwitchCase.EmployeeWageSwitchCase();
-
+            //  EmployeeWageUsingSwitchCase.EmployeeWageSwitchCase();
+            CalculatingWagesForaMonth.WagesforMonth();
         }
     }
 }
